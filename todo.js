132b59dafc9ee528a65bbe4040fc2f885bc919d1
Phen Thach <pshannonthach@gmail.com>
2019-08-27 20:59:43 -0400

    adding todo functions using javascript

---
+++ todo.js
@@ -0,0 +1,25 @@
+var todos = ['item 1', 'item2','item3'];
+
+// It should have a function to display todos.
+//
+function displayTodos(){
+    console.log('my todos:', todos);
+}
+
+//It should have a function to add todos.
+function addTodo(todo){
+    todos.push(todo);
+    displayTodos();
+}
+
+//It shoould have a function to change todos.
+function changeTodo (position, newValue){
+    todos[position] = newValue;
+    displayTodos();
+}
+
+//It should have a function to delete todos.
+function deleteTodo(position){
+    todos.splice(position,1);
+    displayTodos();
+}
