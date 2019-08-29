53ca4d8e67f5d01b6504d4e3f0cf45152ed47a20
Phen Thach <pshannonthach@gmail.com>
2019-08-29 18:07:53 -0400

    changed to an object instead with 4 methods

--- todo.js
+++ todo.js
@@ -1,25 +1,18 @@
-var todos = ['item1', 'item2','item3'];
-
-// It should have a function to display todos.
-//
-function displayTodos(){
-    console.log('my todos:', todos);
+var todoList = {
+    todos: ['item 1', 'item 2', 'item 3'],
+    displayTodos: function () {
+    console.log("My todos", this.todos);
+},
+addTodo: function (todo){
+    this.todos.push(todo);
+    this.displayTodos();
+},
+changeTodo: function (position, newValue){
+    this.todos[position] = newValue;
+    this.displayTodos();
+},
+deleteTodo: function (position){
+    this.todos.splice(position, 1);
+    this.displayTodos();
 }
-
-//It should have a function to add todos.
-function addTodo(todo){
-    todos.push(todo);
-    displayTodos();
-}
-
-//It shoould have a function to change todos.
-function changeTodo (position, newValue){
-    todos[position] = newValue;
-    displayTodos();
-}
-
-//It should have a function to delete todos.
-function deleteTodo(position){
-    todos.splice(position,1);
-    displayTodos();
-}
+};
