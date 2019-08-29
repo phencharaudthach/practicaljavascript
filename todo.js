eea92be5a93c3086d2fc3c233afdc0bbb44582e9
Phen Thach <pshannonthach@gmail.com>
2019-08-29 19:57:00 -0400

    refactoring the buttons and call method

--- todo.js
+++ todo.js
@@ -60,13 +60,11 @@ toggleAll: function(){
 }
 };
 
-var displayTodosButton = document.getElementById('displayTodosButton');
-var toggleAllButton = document.getElementById('toggleAllButton');
-
-displayTodosButton.addEventListener('click', function() {
-todoList.displayTodos();
-});
-
-toggleAllButton.addEventListener('click', function(){
-    todoList.toggleAll();
-})
+var handlers = {
+    displayTodos: function(){
+        todoList.displayTodos();
+    },
+    toggleAll: function(){
+        todoList.toggleAll();
+    }
+};
